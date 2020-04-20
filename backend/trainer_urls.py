7aff361301ec78fc73d9cffb8b406f5939e0d11c
ShakilAhmmed <shakilfci461@gmail.com>
2020-04-21 01:48:19 +0600

    Trainer

--- backend/trainer_urls.py
+++ backend/trainer_urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import trainer_views
 
 urlpatterns = [
-    path('', trainer_views.trainer, name="trainer"),
-    # path('<int:pk>/delete', course_views.course_delete, name="course_delete"),
-    # path('<int:pk>/status', course_views.course_status_change, name="course_status_change"),
-    # path('<int:pk>/edit', course_views.course_edit, name="course_edit")
+	path('', trainer_views.trainer, name = "trainer"),
+	# path('<int:pk>/delete', course_views.course_delete, name="course_delete"),
+	# path('<int:pk>/status', course_views.course_status_change, name="course_status_change"),
+	# path('<int:pk>/edit', course_views.course_edit, name="course_edit")
 ]
